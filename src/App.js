f6b404571a099deeb3088220992dacc8a2267d52
Vahagn Zaqaryan <vahagn.zaqaryan@tumo.org>
2018-06-24 15:07:11 +0400

    Create layout :)

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import{
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
