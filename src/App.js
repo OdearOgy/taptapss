15960bbc9c90fb5890855b1af2e5214b264d1dc6
Vahagn Zaqaryan <vahagn.zaqaryan@tumo.org>
2018-07-11 11:23:19 +0400

    Nav-Bar constraction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Tss.css';
+import Banner from './img/banner.jpg';
 import{
     BrowserRouter as Router,
     Route,
     Link
 } from 'react-router-dom';
+var banner = {
+    backgroundImage: `url(${Banner})`
+}
 class App extends Component {
-
     render() {
         return (
             <Router>
-                <div className='main'>
-                    <div className=''>
+                <div className='Main'>
+                    <div className='Header w--full h--full' style={ banner }>
+                        <div className='Header-cover w--full h--full'>
+                            <div className='Nav w--full m--0'>
+                                <div className='Nav-inner'>
+                                    <div className='Logo d--inline-block f--left'>
 
+                                    </div>
+                                    <div className='Menu d--inline-block f--right'>
+                                        <ul className='.p--0 .m--0'>
+                                            <li className='d--inline-block l-style--none'><a href='#' className='d-block text-d--none text-t--up'>home</a></li>
+                                            <li className='d--inline-block l-style--none'><a href='#' className='d-block text-d--none text-t--up'>blog</a></li>
+                                            <li className='d--inline-block l-style--none'><a href='#' className='d-block text-d--none text-t--up'>about</a></li>
+                                            <li className='d--inline-block l-style--none'><a href='#' className='d-block text-d--none text-t--up'>service</a></li>
+                                            <li className='d--inline-block l-style--none'><a href='#' className='d-block text-d--none text-t--up'>content</a></li>
+                                            <li className='d--inline-block l-style--none'><a href='#' className='d-block text-d--none text-t--up'>faq</a></li>
+                                        </ul>
+                                    </div>
+                                    <div className='clear'></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Router>
